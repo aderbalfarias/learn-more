031a2f946d0930b399635b44b74c2eb28e9f17eb
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-10 23:28:47 +0100

    check-in initial

---
+++ LearnMore.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnMore.Mvc.Startup))]
+namespace LearnMore.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
