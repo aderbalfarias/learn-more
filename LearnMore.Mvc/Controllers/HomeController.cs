5ed23f763e1e60506662397888329ece2fbe3c59
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-10 23:32:53 +0100

    test

--- LearnMore.Mvc/Controllers/HomeController.cs
+++ LearnMore.Mvc/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace LearnMore.Mvc.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "test";
 
             return View();
         }
