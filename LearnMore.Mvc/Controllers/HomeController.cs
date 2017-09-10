dbbcb62022aae5df4b511d05b0c6368d801e486c
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-10 23:44:30 +0100

    test

--- LearnMore.Mvc/Controllers/HomeController.cs
+++ LearnMore.Mvc/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace LearnMore.Mvc.Controllers
 {
@@ -22,7 +18,7 @@ namespace LearnMore.Mvc.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "";
 
             return View();
         }
