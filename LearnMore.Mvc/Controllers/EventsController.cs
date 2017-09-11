814c287961985224bc1752b790e9a02c84df9e57
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-11 23:43:30 +0100

    Create form Event

---
+++ LearnMore.Mvc/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using LearnMore.Mvc.Models;
+using LearnMore.Mvc.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LearnMore.Mvc.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Event
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ LearnMore.Mvc/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LearnMore.Mvc.Models;
+using System.Collections.Generic;
+
+namespace LearnMore.Mvc.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
