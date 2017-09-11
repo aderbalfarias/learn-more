764fcc05545e6f82f6d50ffcc1422ec79f9ce2ed
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-11 21:28:46 +0100

    Populate Genres

---
+++ LearnMore.Mvc/Migrations/201709112017569_PopulateGenresTable.cs
@@ -0,0 +1,21 @@
+namespace LearnMore.Mvc.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateGenresTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("insert into Genres (Id, Name) values (1, 'Art')");
+            Sql("insert into Genres (Id, Name) values (2, 'Sport')");
+            Sql("insert into Genres (Id, Name) values (3, 'Technology')");
+            Sql("insert into Genres (Id, Name) values (4, 'Culture')");
+            Sql("insert into Genres (Id, Name) values (5, 'Science')");
+        }
+
+        public override void Down()
+        {
+            Sql("delete from Genres whete Id in (1, 2, 3, 4, 5)");
+        }
+    }
+}
